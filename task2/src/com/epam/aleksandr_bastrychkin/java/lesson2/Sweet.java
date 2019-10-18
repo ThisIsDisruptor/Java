e3300e8c0bd82870c24c036de18215a467b00f4a
Aleksandr Bastrychkin <a_s_bas@mail.ru>
2019-10-18 10:48:24 +0300

    Sweet is comparable

--- task2/src/com/epam/aleksandr_bastrychkin/java/lesson2/Sweet.java
+++ task2/src/com/epam/aleksandr_bastrychkin/java/lesson2/Sweet.java
@@ -1,8 +1,12 @@
 package com.epam.aleksandr_bastrychkin.java.lesson2;
 
-public abstract class Sweet {
+public abstract class Sweet implements Comparable <Sweet> {
     private String name;
     private double weight;
+
+    public int compareTo(Sweet s){
+        return (int) (weight - s.weight);
+    }
 
 
 }
